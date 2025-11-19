378f351b169f74d737f2c674d03a9868e58df289
jungbauer-1968 <ah@elektro-jungbauer.at>
2025-11-19 09:58:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-function sendForm() {
-    alert("Formular am Handy ausgefüllt.\n\nDu kannst jetzt z.B. einen Screenshot machen oder die Werte ins Büro weitergeben.");
+async function sendForm() {
+    const data = {
+        monteur: document.getElementById("monteur").value,
+        baustelle: document.getElementById("baustelle").value,
+        details: document.getElementById("details").value,
+        prozent: document.getElementById("prozent").value,
+        regie: document.getElementById("regie").value,
+        bemerkungen: document.getElementById("bemerkungen").value
+    };
+
+    await fetch("https://script.google.com/macros/s/AKfycbwNmQr8j3CqAmDW67FZnLiA2BtedtPYfiVSC2l_9wWVuT-QJK9-RAvAqRIGC-H1D_pg/exec", {
+        method: "POST",
+        mode: "no-cors",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data)
+    });
+
+    alert("Bericht erfolgreich gesendet!");
 }
