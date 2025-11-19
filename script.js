0060d2fd10ee9432775c6872cb46fc7711f6b59c
jungbauer-1968 <ah@elektro-jungbauer.at>
2025-11-19 13:44:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,11 +8,13 @@ async function sendForm() {
         details: document.getElementById("details").value,
         regie: document.getElementById("regie").value,
         probleme: document.getElementById("probleme").value,
-        koordination: document.getElementById("koordination").value
+        koordination: document.getElementById("koordination").value,
+        unterschrift: document.getElementById("unterschrift").value
     };
 
     await fetch("https://script.google.com/macros/s/AKfycbzqMHKmHS4gYQB_3CWGA3TH6nj7jqy58CCNZiVIIW-NjTqCh1XfbGVKMUddH3yPo6ak/exec", {
         method: "POST",
+        mode: "no-cors",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data)
     });
